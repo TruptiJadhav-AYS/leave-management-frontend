ea33352ec424aa3233a50403cbccc3489fc9d26d
Pruthviraj Suryawanshi <pruthaviraj.suryawanshi@ayssoftwaresolution.com>
2024-04-03 19:37:52 +0100

    "Holidays warning Resolved"

--- src/components/Holidays.jsx
+++ src/components/Holidays.jsx
@@ -21,7 +21,7 @@ export default function Holidays(){
         container
         justifyContent={"center"}
         >
-        <Grid sx={{ height:"100vh" , pt:"10vh"}}  xs={12} sm={6}>
+        <Grid item sx={{ height:"100vh" , pt:"10vh"}}  xs={12} sm={6}>
             <List  sx={{overflow:"auto",width :"100%"}}>
             {annualLeaves.map((holiday,index) => (
                 <ListItem key={index} sx={{backgroundColor: "#fafafa" ,py:"12px",borderRadius:"10px",m:"3px"}}>
