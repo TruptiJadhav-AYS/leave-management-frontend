f21e52aa80881a4b18aa5a256e5f8582a24927e0
Pratiksha Nimbalkar <pratiksha.nimbalkar@ayssoftwaresolution.com>
2024-04-07 23:10:07 +0100

    Leave Request Table Added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Grid, Typography, Card, CardContent, Divider,Button } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  Divider,
+  Button,
+} from "@mui/material";
 import UseReponsive from "../hooks/UseResponsive";
 import { Gauge, gaugeClasses } from "@mui/x-charts";
 import List from "@mui/material/List";
@@ -7,41 +14,63 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import ListItem from "@mui/material/ListItem";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+// import { useNavigate } from "react-router-dom";
 
 export default function Dashboard({ role }) {
   const responsive = UseReponsive();
-  function createData(name, fromDate, toDate, leaveType, reason,status) {
-    return {name, fromDate, toDate, leaveType, reason,status};
+  // const navigate=useNavigate()
+  function createData(name, fromDate, toDate, leaveType, reason, status) {
+    return { name, fromDate, toDate, leaveType, reason, status };
   }
-  
+
   const rows = [
-    createData('Pratiksha', "10-04-2024", "10-04-2024", "Half Day", "Doctor appointment"),
-    createData('Trupti', "10-04-2024", "10-04-2024", "Half Day", "Doctor appointment"),
-    createData('Pruthvi', "10-04-2024", "10-04-2024", "Half Day", "Doctor appointment"),
-    createData('Prerana', "10-04-2024", "10-04-2024", "Half Day", "Doctor appointment"),
+    createData(
+      "Pratiksha",
+      "10-04-2024",
+      "10-04-2024",
+      "Half Day",
+      "Doctor appointment"
+    ),
+    createData(
+      "Trupti",
+      "10-04-2024",
+      "10-04-2024",
+      "Half Day",
+      "Doctor appointment"
+    ),
+    createData(
+      "Pruthvi",
+      "10-04-2024",
+      "10-04-2024",
+      "Half Day",
+      "Doctor appointment"
+    ),
+    createData(
+      "Prerana",
+      "10-04-2024",
+      "10-04-2024",
+      "Half Day",
+      "Doctor appointment"
+    ),
   ];
-  
+
   const handleAccept = (name) => {
     console.log("Accepted", name);
-    
   };
-  
+
   const handleReject = (name) => {
     console.log("Rejected", name);
-    
   };
 
   return (
-    <Grid container width="100%" p={1}>
+    <Grid container width="100%" p={1} sx={{mt:responsive.isMobile ? "10px":"0px" }}>
       <Grid width="100%" height={"100%"}>
         <Grid container spacing={1} height={"17vh"}>
           <Grid item xs={4} sm={4} md={4} lg={4}>
@@ -143,20 +172,25 @@ export default function Dashboard({ role }) {
           container
           pt={2}
           height={"50%"}
-          sx={{ mt: responsive.isMobile ? "10px" : "0px" }}
+          sx={{ mt: responsive.isMobile ? "35px" : "0px" }}
           rowSpacing={responsive.isMobile ? 1 : 3}
           columnSpacing={1}
         >
           <Grid
             item
             xs={12}
+            // sx={{mt:responsive.isMobile ? "10px":"0px" }}
             sm={role === "manager" || role === "Admin" ? 3 : 6}
             md={role === "manager" || role === "Admin" ? 3 : 6}
             lg={role === "manager" || role === "Admin" ? 3 : 6}
           >
             <Card>
               <CardContent>
-                <Typography fontWeight={"bold"} textAlign={"left"} fontSize={"16px"}>
+                <Typography
+                  fontWeight={"bold"}
+                  textAlign={"left"}
+                  fontSize={"16px"}
+                >
                   Upcoming Holidays
                 </Typography>
               </CardContent>
@@ -268,66 +302,90 @@ export default function Dashboard({ role }) {
             <Grid item xs={12} sm={9} md={9} lg={9}>
               <Card>
                 <CardContent>
-                  <Typography fontWeight={"bold"} textAlign={"left"} fontSize={"16px"}>
+                  <Typography
+                    fontWeight={"bold"}
+                    textAlign={"left"}
+                    fontSize={"16px"}
+                  >
                     Pending Requests
                   </Typography>
                 </CardContent>
                 <Divider />
                 <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 680 }} aria-label="simple table">
-        <TableHead>
-          <TableRow >
-            <TableCell sx={{ fontWeight: 'bold' }}>Name</TableCell>
-            <TableCell align="center" sx={{ fontWeight: 'bold' }}>From date</TableCell>
-            <TableCell align="center" sx={{ fontWeight: 'bold' }}>To date</TableCell>
-            <TableCell align="center" sx={{ fontWeight: 'bold' }}>Leave Type</TableCell>
-            <TableCell align="center" sx={{ fontWeight: 'bold' }}>Reason</TableCell>
-            <TableCell align="center" sx={{ fontWeight: 'bold' }}>Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.fromDate}</TableCell>
-              <TableCell align="right">{row.toDate}</TableCell>
-              <TableCell align="right">{row.leaveType}</TableCell>
-              <TableCell align="right">{row.reason}</TableCell>
-              <TableCell align="right"><Button 
-          variant="contained" 
-          color="success" 
-          size="small" 
-          onClick={() => handleAccept(row.name)}
-          sx={{ marginRight: 1 }}
-        >
-          Accept
-        </Button>
-        <Button 
-          variant="outlined" 
-          color="error" 
-          size="small" 
-          onClick={() => handleReject(row.name)}
-        >
-          Reject
-        </Button></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-                
+                  <Table sx={{ minWidth: 680 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell sx={{ fontWeight: "bold" }}>Name</TableCell>
+                        <TableCell align="center" sx={{ fontWeight: "bold" }}>
+                          From date
+                        </TableCell>
+                        <TableCell align="center" sx={{ fontWeight: "bold" }}>
+                          To date
+                        </TableCell>
+                        <TableCell align="center" sx={{ fontWeight: "bold" }}>
+                          Leave Type
+                        </TableCell>
+                        <TableCell align="center" sx={{ fontWeight: "bold" }}>
+                          Reason
+                        </TableCell>
+                        <TableCell align="center" sx={{ fontWeight: "bold" }}>
+                          Status
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow
+                          key={row.name}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                            overflow: "scroll",
+                          }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {row.name}
+                          </TableCell>
+                          <TableCell align="right">{row.fromDate}</TableCell>
+                          <TableCell align="right">{row.toDate}</TableCell>
+                          <TableCell align="right">{row.leaveType}</TableCell>
+                          <TableCell align="right">{row.reason}</TableCell>
+                          <TableCell align="right">
+                            <Button
+                              variant="contained"
+                              color="success"
+                              size="small"
+                              onClick={() => handleAccept(row.name)}
+                              sx={{ marginRight: 1 ,mb:responsive.isMobile ? "10px":"0px" }}
+                              
+                            >
+                              Accept
+                            </Button>
+                            <Button
+                              variant="outlined"
+                              color="error"
+                              size="small"
+                              onClick={() => handleReject(row.name)}
+                              sx={{mr:responsive.isMobile ? "10px":"0px" }}
+                            >
+                              Reject
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               </Card>
             </Grid>
           ) : (
             <Grid item xs={12} sm={6} md={6} lg={6}>
               <Card>
                 <CardContent>
-                  <Typography fontWeight={"bold"} fontSize={"16px"} textAlign={"left"}>
+                  <Typography
+                    fontWeight={"bold"}
+                    fontSize={"16px"}
+                    textAlign={"left"}
+                  >
                     Approver
                   </Typography>
                 </CardContent>
