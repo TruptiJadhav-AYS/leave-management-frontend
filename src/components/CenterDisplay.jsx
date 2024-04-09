4f0326d2294e55f6715e60e6375a549408cd6783
Pruthviraj Suryawanshi <pruthaviraj.suryawanshi@ayssoftwaresolution.com>
2024-04-09 17:18:40 +0100

    Responsive EmployeeList

--- src/components/CenterDisplay.jsx
+++ src/components/CenterDisplay.jsx
@@ -2,19 +2,25 @@ import LeaveReqForm from "./leaveReqForm";
 import { Routes, Route } from "react-router-dom";
 import Holidays from "./Holidays";
 import History from "./History";
-
+import UseReponsive from "../hooks/UseResponsive";
 import EmployeeList from "./Employee";
 import EmployeeRegistrationForm from "./EmployeeRegistrationForm";
 import Dashboard from "./Dashboard";
+import EmployeeMobile from "./EmployeeMobile";
 
 export default function CenterDisplay({ role }) {
+  let responsive=UseReponsive()
   return (
     <Routes>
       <Route path="/" element={<Dashboard role={role} />} />
       <Route path="/ApplyLeave" element={<LeaveReqForm />} />
       <Route path="/History" element={<History />} />
       <Route path="/Holidays" element={<Holidays />} />
-      <Route path="/Employees" element={<EmployeeList />} />
+      <Route
+          path="/Employees"
+          element={responsive.isMobile ? <EmployeeMobile /> : <EmployeeList />}
+        />
+      
       <Route
         path="/Employees/NewRegistration"
         element={<EmployeeRegistrationForm />}
